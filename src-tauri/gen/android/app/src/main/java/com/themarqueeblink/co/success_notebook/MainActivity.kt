1fbd8842bf9a80d349aa39133ae542a6fdf29095
Corentin Merot <cmerot@themarqueeblink.com>
2025-10-22 23:15:07 +0200

    feat(android): statusbar color follows system mode

--- src-tauri/gen/android/app/src/main/java/com/themarqueeblink/co/success_notebook/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/themarqueeblink/co/success_notebook/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.themarqueeblink.co.success_notebook
 
+import android.annotation.SuppressLint
+import android.util.Log
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
-    // enableEdgeToEdge()
+    enableEdgeToEdge()
     super.onCreate(savedInstanceState)
+  }
+
+  @SuppressLint("SourceLockedOrientationActivity")
+  override fun onConfigurationChanged(newConfig: Configuration) {
+    super.onConfigurationChanged(newConfig)
+    WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightStatusBars =
+      (newConfig.uiMode and Configuration.UI_MODE_NIGHT_MASK) != Configuration.UI_MODE_NIGHT_YES
   }
 }
